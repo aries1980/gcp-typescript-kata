565760413a98e14ece657b48ffe292310ce3526d
Janos Feher <i.am@janosfeher.com>
2019-03-24 17:39:26 +0000

    .

--- src/birthday/__tests__/gcpBirthdayMiddleware.test.ts
+++ src/birthday/__tests__/gcpBirthdayMiddleware.test.ts
@@ -4,18 +4,18 @@ import { Request, Response, NextFunction } from 'express';
 describe('GCP Birthday Express MiddleWare', () => {
 
   it('should sanitize username', async () => {
-    const sanitizedUsername1 = () => { return extractUsername('&foo') };
-    const sanitizedUsername2 = () => { return extractUsername('foo123,') };
-    const sanitizedUsername3 = () => { return extractUsername(' spaces ,') };
+    const sanitizedUsername1 = () => { return extractUsername('&foo'); };
+    const sanitizedUsername2 = () => { return extractUsername('foo123,'); };
+    const sanitizedUsername3 = () => { return extractUsername(' spaces ,'); };
 
     expect(sanitizedUsername1()).toEqual('foo');
     expect(sanitizedUsername2()).toEqual('foo123');
     expect(sanitizedUsername3()).toEqual('spaces');
   });
 
-  it('should throw error on wrong username', async ()=> {
-    const sanitizeUsername = () => { const u = extractUsername('&foo') };
-    const badUsername = () => { const u = extractUsername('&áé') };
+  it('should throw error on wrong username', async () => {
+    const sanitizeUsername = () => { const u = extractUsername('&foo'); };
+    const badUsername = () => { const u = extractUsername('&áé'); };
 
     expect(badUsername).toThrow(TypeError);
   });
@@ -27,17 +27,17 @@ describe('GCP Birthday Express MiddleWare', () => {
     expect(goodDate2.format('YYYY-MM-DD')).toEqual('1980-04-07');
   });
 
-  it('should throw error on wrong date', async ()=> {
+  it('should throw error on wrong date', async () => {
     const storage = {
       save: jest.fn(() => true)
     };
 
-    const badDate1 = () => { const d = extractDateOfBirth('800-01-01') };
-    const badDate2 = () => { const d = extractDateOfBirth('1980-02-30') };
+    const badDate1 = () => { const d = extractDateOfBirth('800-01-01'); };
+    const badDate2 = () => { const d = extractDateOfBirth('1980-02-30'); };
 
     expect(badDate1).toThrow(TypeError);
     expect(badDate2).toThrow(TypeError);
-    //expect(test_2).toEqual({ url: '/nl-nl/test2' });
+    // expect(test_2).toEqual({ url: '/nl-nl/test2' });
   });
 
 });
